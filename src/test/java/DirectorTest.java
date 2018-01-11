2398c51c44fd4d8306d266c600fe4c0bbeecd7aa
Dave Ellis <dave@ellisnetmail.co.uk>
2018-01-11 15:51:35 +0000

    Added Director class with tests - MVP complete

---
+++ src/test/java/DirectorTest.java
@@ -0,0 +1,20 @@
+import management.Director;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class DirectorTest {
+
+    Director director;
+
+    @Before
+    public void before(){
+        director = new Director("Joe Bloggs", "AB1234567C", 50000.00, "Finance", 100_000.00);
+    }
+
+    @Test
+    public void hasBudget(){
+        assertEquals(100_000.00, director.getBudget(), 0.01);
+    }
+}
